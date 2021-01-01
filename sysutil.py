837365e87d737ad02d75ea20aea52ed57545f83c
jawaddle <hjawaddle@mac.com>
2021-01-01 15:19:57 -0700

    Adapted code from sensors.py code to show temps, fan speed and battery

--- sysutil.py
+++ sysutil.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # adapted from ifconfig.py: 
 #   https://github.com/giampaolo/psutil/blob/master/scripts/ifconfig.py
+# adapteed from sensors.py
+#   https://github.com/giampaolo/psutil/blob/master/scripts/sensors.py
 
 import socket
 import psutil
@@ -14,6 +16,7 @@ CPU_INFO   =   "*                               CPU INFORMATION                 
 MEMORY_INFO =  "*                            MEMORY INFORMATION                                *"
 DISK_INFO   =  "*                              DISK INFORMATION                                *"
 NETWORK_INFO = "*                           NETWORK INFORMATION                                *"
+CLIMATE_INFO = "*                           CLIMATE INFORMATION                                *"
 
 MEGABYTE = 1024 * 1024
 GIGABYTE = 1024 * 1024 * 1024
@@ -146,6 +149,63 @@ def get_network_info():
     print(SECTION_SPAN)    
     return
 
+def secs2hours(secs):
+    mm, ss = divmod(secs, 60)
+    hh, mm = divmod(mm, 60)
+    return "%d:%02d:%02d" % (hh, mm, ss)
+
+
+def get_hw_temperatures():
+    """"""
+
+    print_section_header(CLIMATE_INFO)
+    if hasattr(psutil, "sensors_temperatures"):
+        temps = psutil.sensors_temperatures()
+    else:
+        temps = {}
+    if hasattr(psutil, "sensors_fans"):
+        fans = psutil.sensors_fans()
+    else:
+        fans = {}
+    if hasattr(psutil, "sensors_battery"):
+        battery = psutil.sensors_battery()
+    else:
+        battery = None
+
+    if not any((temps, fans, battery)):
+        print("Cannot read temperature, fans or battery info")
+        return
+
+    names = set(list(temps.keys()) + list(fans.keys()))
+    for name in names:
+        print(name)
+        # Temperatures.
+        if name in temps:
+            print("    Temperatures:")
+            for entry in temps[name]:
+                print("        %-20s %s°C (high=%s°C, critical=%s°C)" % (
+                    entry.label or name, entry.current, entry.high,
+                    entry.critical))
+        # Fans.
+        if name in fans:
+            print("    Fans:")
+            for entry in fans[name]:
+                print("        %-20s %s RPM" % (
+                    entry.label or name, entry.current))
+
+    # Battery.
+    if battery:
+        print("Battery:")
+        print("    charge:     %s%%" % round(battery.percent, 2))
+        if battery.power_plugged:
+            print("    status:     %s" % (
+                "charging" if battery.percent < 100 else "fully charged"))
+            print("    plugged in: yes")
+        else:
+            print("    left:       %s" % secs2hours(battery.secsleft))
+            print("    status:     %s" % "discharging")
+            print("    plugged in: no")
+    print(SECTION_SPAN) 
 
 if __name__ == "__main__":
     print('\n') # put a blank line after command line 
@@ -154,4 +214,6 @@ if __name__ == "__main__":
     get_memory_info()
     get_disk_info()
     get_network_info()
+    get_hw_temperatures()
+
 
